7f24620c953e59a3a14838c29b4efeb7aaa1436b
syeds17 <syedsadath7777777@gmail.com>
2024-03-13 16:42:16 +0530

    uploding file-2

--- BinomialCoefficient.py
+++ BinomialCoefficient.py
@@ -1,1 +1,15 @@
 """Developing a python program to calculate binomial coefficient (Given N and R)"""
+
+def fact(num):  #Defining a function to calculate factorial of a number
+    if num == 0:
+        return 1
+    else:
+        return num * fact(num-1)
+#Input from user    
+n = int(input("Enter the value of N : ")) 
+r = int(input("Enter the value of R : "))
+assert  r < n,  "R cannot be greater than N"  #Raising an assertion
+if r<=0:
+    print("R cannot be less than or equal to zero")
+nCr = fact(n)/(fact(r)*fact(n-r))
+print(n,'C',r,"=","%d"%nCr,sep="")
