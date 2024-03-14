6bd1c90785c9db7ec20ce8183515b0514eb66cb6
syeds17 <syedsadath7777777@gmail.com>
2024-03-14 19:17:42 +0530

    uploading

---
+++ Mathamatical-programs/Simple-Graph.py
@@ -0,0 +1,17 @@
+"""A Simple Graph"""          #For better output use Python 3.12.1 (IDLE) Shell.....
+
+import mathplotlib.pyplot as plt   #Importing modules
+import numpy as np
+
+x = np.linspace(0,2,100)
+
+plt.plot(x,x,label="linear")
+plt.plot(x,x**2,label="quadratic")   #ploting the curves
+plt.plot(x,x**3,label="cubic")
+
+plt.xlabel("x-label")
+plt.ylabel("y-label")
+plt.title("Simple plot")   #labeling graph
+plt.legend()
+plt.show()
+
